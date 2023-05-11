a716d40c2b24e51c98f210fd8ffcb609f40a9dd1
Ramla 🌺 <ramla@Ramlas-MBP.lan>
2023-05-10 22:58:10 -0600

    created hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,12 @@ module.exports = {
     },
     colors: {
       "snow": "#fbf5f3",
-      "fulvous": "#e28413",
-      "oxford-blue": "#000022",
-      "rusty-red": "#de3c4b",
-      "cardinal" : "#c42847",
       "orchid": "#faa6ff",
       "violet": "#7353ba",
       "violet-dark": "#2f195f",
       "plum": "#0f1020",
-      "purple-light": "#efc3f5"
+      "purple-light": "#efc3f5",
+      "purple-gray":"#4E3A5F"
 
     },
     extend: {},
